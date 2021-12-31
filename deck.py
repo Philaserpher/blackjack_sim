6e2b9a730069ab1df996cc9ff3589feaba49039e
Jpfgd <jpm217@cam.ac.uk>
2021-12-31 19:36:05 +0000

    Added basic main function

--- deck.py
+++ deck.py
@@ -2,11 +2,13 @@ import random
 
 class Deck:
     NORMAL_DECK = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]*4
+
     def __init__(self, length):
         self.cards = Deck.NORMAL_DECK*length
+
     def shuffle(self):
         random.shuffle(self.cards)
-        
+
     def get_card(self):
         return(self.cards.pop(0))
 
@@ -15,3 +17,4 @@ class Deck:
 
 
 
+        
--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from deck import Deck
 from player import getplay
 
+#Set up constants
+ITERATIONS = 1000
+
 deck = Deck(4)
-deck.show()
+deck.shuffle()
+
+
+def play():
+    player_cards, dealer_cards = [], []
+    for i in range(2):
+        player_cards.append(deck.get_card())
+        dealer_cards.append(deck.get_card())
+    print(player_cards)
+    print(dealer_cards)
+
+
+play()
