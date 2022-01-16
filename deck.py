c03044833a5683376c160d3ad05e65beca2e1ea7
Philaserpher <jpfgd20@gmail.com>
2022-01-16 16:40:50 +0000

    fixed format v2

--- deck.py
+++ deck.py
@@ -1,26 +1,26 @@
 import random
 
 
-
 class Deck:
 
-    NORMAL_DECK = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]*4        # normal card deck, no jacks
+    NORMAL_DECK = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10,
+                   10, 11]*4        # normal card deck, no jacks
 
     def __init__(self, n):
-        self.cards = Deck.NORMAL_DECK*n                                 # put n decks in the shoe
-    
+        # put n decks in the shoe
+        self.cards = Deck.NORMAL_DECK*n
+
     def shuffle(self):
         random.shuffle(self.cards)
 
     def get_card(self):
-        return(self.cards.pop(0))                                       # return top card, remove from deck (essentially dealing card)
+        # return top card, remove from deck (essentially dealing card)
+        return(self.cards.pop(0))
 
     def show(self):
         print(self.cards)
 
     def add_decks(self, n):
-        self.cards += Deck.NORMAL_DECK*n                                # refill the shoe with n decks
+        # refill the shoe with n decks
+        self.cards += Deck.NORMAL_DECK*n
         self.shuffle()
-
-
-
